49997b970a6f2940fd72970359196d0e91e83664
mfrozi <m.fakhrulrozi10@gmail.com>
2022-10-04 23:09:24 +0700

    add destination_certifications table

---
+++ database/migrations/2022_10_04_160026_create_destination_certifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destination_certifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('destination_id');
+            $table->string('certification_name');
+            $table->string('certification_badge');
+            $table->timestamp('acquired_date');
+            $table->timestamp('expiration_date');
+            $table->string('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destination_certifications');
+    }
+};
