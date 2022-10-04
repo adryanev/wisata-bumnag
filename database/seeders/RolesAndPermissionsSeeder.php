e8ef16451fad746cf7b54a82e79ada0dbd88e210
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-10-04 16:06:22 +0000

    edit permission seeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -22,44 +22,44 @@ class RolesAndPermissionsSeeder extends Seeder
 
         // ===== ALL PERMISSION =====
         // PERMISSION Penilaian Kegiatan
-        Permission::create(['name' => PermissionConstants::EDIT_PENILAIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::DELETE_PENILAIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::CREATE_PENILAIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::READ_PENILAIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::EDIT_PENILAIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::DELETE_PENILAIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::CREATE_PENILAIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::READ_PENILAIAN_KEGIATAN]);
 
         // PERMISSION Pengisian Kegiatan
-        Permission::create(['name' => PermissionConstants::EDIT_ALL_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::EDIT_OWN_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::CREATE_ALL_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::CREATE_OWN_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::DELETE_ALL_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::DELETE_OWN_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::READ_ALL_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::READ_OWN_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::VALIDATE_PENGISIAN_KEGIATAN]);
-        Permission::create(['name' => PermissionConstants::COMMENT_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::EDIT_ALL_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::EDIT_OWN_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::CREATE_ALL_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::CREATE_OWN_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::DELETE_ALL_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::DELETE_OWN_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::READ_ALL_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::READ_OWN_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::VALIDATE_PENGISIAN_KEGIATAN]);
+        // Permission::create(['name' => PermissionConstants::COMMENT_PENGISIAN_KEGIATAN]);
 
 
         $role = Role::create(['name'=> 'admin']);
         $role->givePermissionTo([
-            PermissionConstants::EDIT_PENILAIAN_KEGIATAN,
-            PermissionConstants::DELETE_PENILAIAN_KEGIATAN,
-            PermissionConstants::CREATE_PENILAIAN_KEGIATAN,
-            PermissionConstants::READ_PENILAIAN_KEGIATAN,
-            PermissionConstants::EDIT_ALL_PENGISIAN_KEGIATAN,
-            PermissionConstants::CREATE_ALL_PENGISIAN_KEGIATAN,
-            PermissionConstants::DELETE_ALL_PENGISIAN_KEGIATAN,
-            PermissionConstants::READ_ALL_PENGISIAN_KEGIATAN,
-            PermissionConstants::VALIDATE_PENGISIAN_KEGIATAN,
-            PermissionConstants::COMMENT_PENGISIAN_KEGIATAN,
+            // PermissionConstants::EDIT_PENILAIAN_KEGIATAN,
+            // PermissionConstants::DELETE_PENILAIAN_KEGIATAN,
+            // PermissionConstants::CREATE_PENILAIAN_KEGIATAN,
+            // PermissionConstants::READ_PENILAIAN_KEGIATAN,
+            // PermissionConstants::EDIT_ALL_PENGISIAN_KEGIATAN,
+            // PermissionConstants::CREATE_ALL_PENGISIAN_KEGIATAN,
+            // PermissionConstants::DELETE_ALL_PENGISIAN_KEGIATAN,
+            // PermissionConstants::READ_ALL_PENGISIAN_KEGIATAN,
+            // PermissionConstants::VALIDATE_PENGISIAN_KEGIATAN,
+            // PermissionConstants::COMMENT_PENGISIAN_KEGIATAN,
         ]);
 
         $role = Role::create(['name'=>'dosen']);
         $role->givePermissionTo([
-            PermissionConstants::EDIT_OWN_PENGISIAN_KEGIATAN,
-            PermissionConstants::CREATE_OWN_PENGISIAN_KEGIATAN,
-            PermissionConstants::DELETE_OWN_PENGISIAN_KEGIATAN,
-            PermissionConstants::READ_OWN_PENGISIAN_KEGIATAN,
+            // PermissionConstants::EDIT_OWN_PENGISIAN_KEGIATAN,
+            // PermissionConstants::CREATE_OWN_PENGISIAN_KEGIATAN,
+            // PermissionConstants::DELETE_OWN_PENGISIAN_KEGIATAN,
+            // PermissionConstants::READ_OWN_PENGISIAN_KEGIATAN,
         ]);
 
         // Give all Permission to superadmin;
