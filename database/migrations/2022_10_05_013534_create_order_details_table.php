c980bfe17d760e2e7055d62b1081f6c7392c0ea6
mfrozi <m.fakhrulrozi10@gmail.com>
2022-10-05 08:49:15 +0700

    add order_detail table

---
+++ database/migrations/2022_10_05_013534_create_order_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('ticket_id');
+            $table->string('name');
+            $table->double('ticket_price');
+            $table->double('quantity');
+            $table->double('subtotal');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
