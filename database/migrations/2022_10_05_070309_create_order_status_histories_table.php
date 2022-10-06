924aa0f95811e7a14f218cac6a6dd0cd96df2ba2
mfrozi <m.fakhrulrozi10@gmail.com>
2022-10-06 20:44:39 +0700

    change enum to tinyinteger

--- database/migrations/2022_10_05_070309_create_order_status_histories_table.php
+++ database/migrations/2022_10_05_070309_create_order_status_histories_table.php
@@ -16,7 +16,12 @@ return new class extends Migration
         Schema::create('order_status_histories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('order_id');
-            $table->enum('status',['created','processed','compeleted','cancelled']);
+            $table->tinyInteger('status',false,true)->comment(
+  '0: created
+            1:paid
+            2:cancelled
+            3:compeleted
+            4:refunded');
             $table->string('descriptions');
             $table->timestamp('create_date');
             $table->timestamps();
