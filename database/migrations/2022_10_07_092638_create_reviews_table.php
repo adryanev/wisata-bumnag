a45923c542ec28f9bf9927203a0e6fb8ae1b6822
mfrozi <m.fakhrulrozi10@gmail.com>
2022-10-07 16:44:07 +0700

    add reviews table

---
+++ database/migrations/2022_10_07_092638_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('reviewable');
+            $table->integer('review');
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
