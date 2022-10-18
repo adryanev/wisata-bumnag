c41af84f022367fe3f0c04a73841cb57bf34d93c
mfrozi <m.fakhrulrozi10@gmail.com>
2022-10-18 12:11:59 +0700

    change openingHours and Closing hours to time type

---
+++ database/migrations/2022_10_18_045819_alter_table_destinations_change_opening_hours_and_closing_hours.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('destinations', function (Blueprint $table) {
+            $table->time('opening_hours')->change();
+            $table->time('closing_hours')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('destinations', function (Blueprint $table) {
+            $table->dateTime('opening_hours')->change();
+            $table->dateTime('closing_hours')->change();
+        });
+    }
+};
