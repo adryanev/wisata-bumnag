3a53b5f03916f0007b5b596fbaa55979a6b15708
mfrozi <m.fakhrulrozi10@gmail.com>
2022-10-23 19:00:29 +0700

    custom path generator media library

---
+++ app/Services/MediaLibrary/CustomPathGenerator.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Services\MediaLibrary;
+
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    /*
+     * Get the path for the given media, relative to the root storage path.
+     */
+    public function getPath(Media $media):string
+    {
+        return class_basename($media->model).'/'.$media->id.'/';
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return class_basename($media->model).'/'.$media->id.'/convertions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return class_basename($media->model).'/'.$media->id.'/responsive-images/';
+    }
+}
