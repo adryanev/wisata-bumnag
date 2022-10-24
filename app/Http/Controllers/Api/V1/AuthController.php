0407ba09c8f2900edc49a1c2f47e844b2371425f
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-10-24 20:41:08 +0700

    change wording login failed

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         $token = JWTAuth::attempt(['email' => $request->email, 'password' => $decoded]);
         if (!$token) {
             return response()->json([
-                'message' => 'Unauthorized',
+                'message' => 'Invalid Email or Password',
             ], 401);
         }
 
