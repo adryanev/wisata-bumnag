98852339c23430209248684e97bf2cad057d9210
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-10-27 20:59:41 +0700

    fix: missing id in destination (#12)

--- app/Http/Resources/DestinationResource.php
+++ app/Http/Resources/DestinationResource.php
@@ -15,6 +15,7 @@ class DestinationResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'address' => $this->address,
