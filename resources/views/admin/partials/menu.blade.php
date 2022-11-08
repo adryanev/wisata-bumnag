3ee7c7f84df3458013551c7fccbddfe54bb3464a
mfrozi <m.fakhrulrozi10@gmail.com>
2022-11-08 11:45:33 +0700

    fix menu

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -4,41 +4,42 @@ $route = (isset($r['as'])) ? $r['as'] : '';
 @endphp
 
 <li class="nav-item mT-30">
-    <a class="sidebar-link {{ Str::startsWith($route, ADMIN . '') ? 'actived' : '' }}" href="{{ route(ADMIN . '.dash') }}">
+    <a class="sidebar-link {{ Str::startsWith($route, 'admin.users') ? 'actived' : '' }}" href="{{ route(ADMIN . '.users.index') }}">
         <span class="icon-holder">
-            <i class="c-blue-500 ti-home"></i>
-        </span>
-        <span class="title">Dashboard</span>
-    </a>
-</li>
-<li class="nav-item">
-    <a class="sidebar-link {{ Str::startsWith($route, ADMIN . 'users') ? 'actived' : '' }}" href="{{ route(ADMIN . '.users.index') }}">
-        <span class="icon-holder">
-            <i class="c-brown-500 ti-user"></i>
+            <i class="
+            {{ Str::startsWith($route, 'admin.users') ? 'c-blue-500 ti-user' : 'c-brown-500 ti-user' }}
+            "></i>
         </span>
         <span class="title">Users</span>
     </a>
 </li>
+
 <li class="nav-item">
-    <a class="sidebar-link {{ Str::startsWith($route, ADMIN . 'destinations') ? 'actived' : '' }}" href="{{ route(ADMIN . '.destinations.index') }}">
+    <a class="sidebar-link {{ Str::startsWith($route, 'admin.destinations') ? 'actived' : '' }}" href="{{ route(ADMIN . '.destinations.index') }}">
         <span class="icon-holder">
-            <i class="c-brown-500 ti-target"></i>
+            <i class="
+            {{ Str::startsWith($route, 'admin.destinations') ? 'c-blue-500 ti-target' : 'c-brown-500 ti-target' }}
+            "></i>
         </span>
         <span class="title">Destinations</span>
     </a>
 </li>
 <li class="nav-item">
-    <a class="sidebar-link {{ Str::startsWith($route, ADMIN . 'adbanners') ? 'actived' : '' }}" href="{{ route(ADMIN . '.adbanners.index') }}">
+    <a class="sidebar-link {{ Str::startsWith($route, 'admin.adbanners') ? 'actived' : '' }}" href="{{ route(ADMIN . '.adbanners.index') }}">
         <span class="icon-holder">
-            <i class="c-brown-500 ti-image"></i>
+            <i class="
+            {{ Str::startsWith($route, 'admin.adbanners') ? 'c-blue-500 ti-image' : 'c-brown-500 ti-image' }}
+            "></i>
         </span>
         <span class="title">Ad Banners</span>
     </a>
 </li>
 <li class="nav-item">
-    <a class="sidebar-link {{ Str::startsWith($route, ADMIN . 'souvenirs') ? 'actived' : '' }}" href="{{ route(ADMIN . '.souvenirs.index') }}">
+    <a class="sidebar-link {{ Str::startsWith($route, 'admin.souvenirs') ? 'actived' : '' }}" href="{{ route(ADMIN . '.souvenirs.index') }}">
         <span class="icon-holder">
-            <i class="c-brown-500 ti-shopping-cart"></i>
+            <i class="
+            {{ Str::startsWith($route, 'admin.souvenirs') ? 'c-blue-500 ti-shopping-cart' : 'c-brown-500 ti-shopping-cart' }}
+            "></i>
         </span>
         <span class="title">Souvenirs</span>
     </a>
