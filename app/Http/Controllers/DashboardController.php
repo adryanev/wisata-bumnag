5d5d9e4e732f35e413a7c9023ea2924d2505852e
mfrozi <m.fakhrulrozi10@gmail.com>
2022-11-14 00:44:07 +0700

    Finalizing UI

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
+use App\Models\Event;
+use App\Models\Package;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        $totalUser = count(User::all());
+        $totalDestination = count(Destination::all());
+        $totalPackage = count(Package::all());
+        $totalEvent = count(Event::all());
+        return view('admin.dashboard.index', compact(
+            'totalUser',
+            'totalDestination',
+            'totalPackage',
+            'totalEvent',
+        ));
     }
 }
