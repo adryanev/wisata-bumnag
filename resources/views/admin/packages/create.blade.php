c5e9056aa7809196422d765a3759a6c23f8a4f40
mfrozi <m.fakhrulrozi10@gmail.com>
2022-11-13 23:33:31 +0700

    Package CRUD Done

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -1,18 +1,18 @@
 @extends('admin.default')
 
 @section('page-header')
-Souvenirs <small><i class="c-white-500 ti-cloud-up"></i></small>
+Packages <small><i class="c-white-500 ti-cloud-up"></i></small>
 
 @stop
 
 @section('content')
 {!! Form::open([
-'route' => [ ADMIN . '.souvenirs.store' ],
+'route' => [ ADMIN . '.packages.store' ],
 'files' => true
 ])
 !!}
 
-@include('admin.souvenirs.form',['categories'=>$categories,'destinations'=>$destinations,'souvenirCategory'=>$souvenirCategory,'souvenirDestination'=>$souvenirDestination,'souvenir'=>$souvenir])
+@include('admin.packages.form',[])
 
 <button type="submit" class="btn btn-primary "><i class="c-white-500 ti-save"></i>&nbsp;Submit</button>
 
