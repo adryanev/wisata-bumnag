6bd1040c7e118c20db6d0356d3d46a6e750a0a6b
mfrozi <m.fakhrulrozi10@gmail.com>
2022-11-14 11:40:53 +0700

    add event softdeletes

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Event extends Model implements HasMedia
 {
-    use HasFactory,InteractsWithMedia;
+    use HasFactory,InteractsWithMedia,SoftDeletes;
 
     protected $fillable = [
         'name',
