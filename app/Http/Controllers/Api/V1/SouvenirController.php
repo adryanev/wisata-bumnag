3213e089a553df4a5421477cd59e249013cc2981
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-11-18 23:59:26 +0700

    feat: souvenir detail (#38)

--- app/Http/Controllers/Api/V1/SouvenirController.php
+++ app/Http/Controllers/Api/V1/SouvenirController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DestinationSouvenirCollection;
 use App\Http\Resources\SouvenirCollection;
+use App\Http\Resources\SouvenirDetailResource;
 use App\Models\Destination;
+use App\Models\Souvenir;
 use Illuminate\Http\Request;
 
 class SouvenirController extends Controller
@@ -19,5 +21,11 @@ class SouvenirController extends Controller
     public function destination(Destination $destination)
     {
         return new SouvenirCollection($destination->souvenirs()->joinRelationship('categories')->available()->get());
+    }
+
+    public function detail($id)
+    {
+        $data = Souvenir::where(['id' => $id])->with('reviews')->firstOrFail();
+        return new SouvenirDetailResource($data);
     }
 }
