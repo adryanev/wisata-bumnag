6c9b0f3ab19ee76969f8f42bc45b1d8f78cc5d7a
mfrozi <m.fakhrulrozi10@gmail.com>
2022-11-21 03:07:23 +0700

    remove media management destination for deploy

--- resources/views/admin/destinations/edit.blade.php
+++ resources/views/admin/destinations/edit.blade.php
@@ -19,46 +19,41 @@ Destination {{ $destination->name }}<small><i class="c-white-500 ti-brush"></i><
 <button type="submit" class="btn btn-primary "><i class="c-white-500 ti-save"></i>&nbsp;Submit</button>
 
 {!! Form::close() !!}
-@if($media != null)
+{{-- @if($media != null)
 
 <div class="col-sm-8 bgc-white mT-20">
     <h5 class="p-15">Destiantion {{ $destination->name }} Photos</h5>
-    <hr>
-    <div class="table-responsive">
-        <table class="table table-striped table-bordered">
-            <thead>
-                <th>Photo</th>
-                <th>Action</th>
-            </thead>
-            <tbody>
-                @foreach ($media as $medias)
-                <tr>
-                    <td>
-                        <img class="img-fluid" src="{{ $medias->original_url }}" alt="Destination {{ $destination->name }} image">
-                    </td>
-                    <td>
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <button class="btn btn-primary"><i class="c-white-500 ti-eye"></i></button>
-                            </li>
-                            <li class="list-inline-item">
-                                <button class="row btn btn-danger"><i class="c-white-500 ti-close" onclick="removeMedia({{ $medias->id }})"></i></button>
-                            </li>
-                        </ul>
-                    </td>
-                </tr>
-                @endforeach
+<hr>
+<div class="table-responsive">
+    <table class="table table-striped table-bordered">
+        <thead>
+            <th>Photo</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            @foreach ($media as $medias)
+            <tr>
+                <td>
+                    <img class="img-fluid" src="{{ $medias->original_url }}" alt="Destination {{ $destination->name }} image">
+                </td>
+                <td>
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <button class="btn btn-primary"><i class="c-white-500 ti-eye"></i></button>
+                        </li>
+                        <li class="list-inline-item">
+                            <button class="row btn btn-danger"><i class="c-white-500 ti-close" onclick="removeMedia({{ $medias->id }})"></i></button>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+            @endforeach
 
 
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
 </div>
-
-
-
-
-
+</div>
 
 @endif
 
@@ -67,5 +62,5 @@ Destination {{ $destination->name }}<small><i class="c-white-500 ti-brush"></i><
         console.log(media);
     }
 
-</script>
+</script> --}}
 @stop
