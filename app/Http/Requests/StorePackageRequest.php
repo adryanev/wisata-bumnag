7c5747139e23a43f5f2ae460ee099447d2ea12d3
mfrozi <m.fakhrulrozi10@gmail.com>
2022-11-24 12:40:06 +0700

    fix: multiple photo Packages

--- app/Http/Requests/StorePackageRequest.php
+++ app/Http/Requests/StorePackageRequest.php
@@ -30,7 +30,8 @@ class StorePackageRequest extends FormRequest
             'activities' => 'required',
             'destination' => 'required',
             'package_category' => 'required',
-            'package_photo' => 'required|image',
+            'package_photo' => 'nullable',
+            'package_photo.*' => 'mimes:png,jpg',
         ];
     }
 }
