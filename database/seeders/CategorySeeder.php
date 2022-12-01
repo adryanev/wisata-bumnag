14b0b72f6ce23e57b8f30e6c4d88f3935d606a77
mfrozi <m.fakhrulrozi10@gmail.com>
2022-12-01 10:48:33 +0700

    Add Paket 1 hari 2 hari 5 hari

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -18,119 +18,131 @@ class CategorySeeder extends Seeder
         $categories = [
             new Category([
                 'name' => 'Wisata',
-            ]),
+            ]), // 1
             new Category([
                 'name' => 'Event',
-            ]),
+            ]), // 2
             new Category([
                 'name' => 'Kuliner',
-            ]),
+            ]), // 3
             new Category([
                 'name' => 'Akomodasi',
-            ]),
+            ]), // 4
             new Category([
                 'name' => 'Desa Wisata',
-            ]),
+            ]), // 5
             //Wisata Alam
             new Category([
                 'name' => 'Wisata Alam',
                 'parent_id' => 1,
-            ]),
+            ]), // 6
             new Category([
                 'name' => 'Wisata Buatan',
                 'parent_id' => 1,
-            ]),
+            ]), // 7
             new Category([
                 'name' => 'Wisata Budaya dan Sejarah',
                 'parent_id' => 1,
-            ]),
+            ]), // 8
             new Category([
                 'name' => 'Wisata Minat Khusus',
                 'parent_id' => 1,
-            ]),
+            ]), // 9
             new Category([
                 'name' => 'Wisata Museum',
                 'parent_id' => 1,
-            ]),
+            ]), // 10
             new Category([
                 'name' => 'Wisata Pantai',
                 'parent_id' => 1,
-            ]),
+            ]), // 11
             //Kuliner
             new Category([
                 'name' => 'Rumah Makan',
                 'parent_id' => 3,
-            ]),
+            ]), // 12
             new Category([
                 'name' => 'Restoran',
                 'parent_id' => 3,
-            ]),
+            ]), // 13
             //Akomodasi
             new Category([
                 'name' => 'Hotel Non Bintang',
                 'parent_id' => 4,
-            ]),
+            ]), // 14
             new Category([
                 'name' => 'Hotel Bintang 1',
                 'parent_id' => 4,
-            ]),
+            ]), // 15
             new Category([
                 'name' => 'Hotel Bintang 2',
                 'parent_id' => 4,
-            ]),
+            ]), // 16
             new Category([
                 'name' => 'Hotel Bintang 3',
                 'parent_id' => 4,
-            ]),
+            ]), // 17
             new Category([
                 'name' => 'Hotel Bintang 4',
                 'parent_id' => 4,
-            ]),
+            ]), // 18
             new Category([
                 'name' => 'Hotel Bintang 5',
                 'parent_id' => 4,
-            ]),
+            ]), // 19
             // Desa Wisata
             new Category([
                 'name' => 'Desa Wisata Alam',
                 'parent_id' => 5,
-            ]),
+            ]), // 20
             new Category([
                 'name' => 'Desa Wisata Kerajinan',
                 'parent_id' => 5,
-            ]),
+            ]), // 21
             //souvenir
             new Category([
                 'name' => 'Souvenir',
-            ]),
+            ]), // 22
             new Category([
                 'name' => 'Makanan dan Minuman',
                 'parent_id' => 22,
-            ]),
+            ]), // 23
             new Category([
                 'name' => 'Kerajinan',
                 'parent_id' => 22,
-            ]),
+            ]), // 24
             //packages
             new Category([
                 'name' => 'Package'
-            ]),
+            ]), // 25
+            new Category([
+                'name' => 'Paket 1 Hari',
+                'parent_id' => 25
+            ]), // 26
+            new Category([
+                'name' => 'Paket 2 Hari',
+                'parent_id' => 25
+            ]), // 27
             new Category([
                 'name' => 'Paket 3 Hari',
                 'parent_id' => 25
-            ]),
+            ]), // 28
+            new Category([
+                'name' => 'Paket 5 Hari',
+                'parent_id' => 25
+            ]), // 29
             new Category([
                 'name' => 'Paket 7 Hari',
                 'parent_id' => 25
-            ]),
+            ]), // 30
             new Category([
                 'name' => 'Paket 14 Hari',
                 'parent_id' => 25
-            ]),
+            ]), // 31
             new Category([
                 'name' => 'Paket 30 Hari',
                 'parent_id' => 25
-            ]),
+            ]), // 32
         ];
         foreach ($categories as $category) {
             $category->save();
