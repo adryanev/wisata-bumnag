f4de375004b8461256577f2b34fe43c39f4581b4
mfrozi <m.fakhrulrozi10@gmail.com>
2023-03-05 23:41:54 +0700

    feat: category crud WIP

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        dd($categories);
     }
 
     /**
