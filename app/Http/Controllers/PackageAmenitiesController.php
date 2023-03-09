ddbeb220031e5e7ce7374b40808f317b2dc4f936
mfrozi <m.fakhrulrozi10@gmail.com>
2023-03-09 16:44:33 +0700

    feat: amenities WIP

---
+++ app/Http/Controllers/PackageAmenitiesController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PackageAmenities;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePackageAmenitiesRequest;
+use App\Http\Requests\UpdatePackageAmenitiesRequest;
+
+class PackageAmenitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePackageAmenitiesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePackageAmenitiesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PackageAmenities  $packageAmenities
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PackageAmenities $packageAmenities)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PackageAmenities  $packageAmenities
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PackageAmenities $packageAmenities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePackageAmenitiesRequest  $request
+     * @param  \App\Models\PackageAmenities  $packageAmenities
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePackageAmenitiesRequest $request, PackageAmenities $packageAmenities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PackageAmenities  $packageAmenities
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PackageAmenities $packageAmenities)
+    {
+        //
+    }
+}
