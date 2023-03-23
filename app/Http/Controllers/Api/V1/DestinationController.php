96186019fd4e4e48878dc69429ff03f2cf2e27c9
mfrozi <m.fakhrulrozi10@gmail.com>
2023-03-23 14:42:33 +0700

    feat: tickets minimum quantitiy 5, amenities too

--- app/Http/Controllers/Api/V1/DestinationController.php
+++ app/Http/Controllers/Api/V1/DestinationController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\DestinationDetailResource;
 use App\Http\Resources\DestinationResource;
 use Illuminate\Http\Request;
 use App\Models\Destination;
+use Illuminate\Contracts\Database\Query\Builder;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -16,7 +17,9 @@ class DestinationController extends Controller
     public function index(Request $request)
     {
         $id = $request->query('category');
-        $destinationPaginator = Destination::category($id)->with(['reviews', 'tickets'])->latest('created_at')->paginate(10);
+        $destinationPaginator = Destination::category($id)->with(['reviews'])->wherehas('tickets', function (Builder $builder) {
+            return $builder->where('quantity', '>', 5);
+        })->latest('created_at')->paginate(10);
         return new DestinationCollection($destinationPaginator);
 
         // $destionation = QueryBuilder::for(Destination::class)
